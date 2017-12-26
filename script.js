e219ae03a70f803f34cf8d3c36ab92e4238a752e
Souvik Das <souvik.das@live.in>
2017-12-26 18:43:44 +0530

    Bugs Fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var completedTasks = [];
 
 var newName;
 
-var todoCounts=0;
+var todoCounts = 0;
 
 var fVal = document.getElementById("task");
 
@@ -13,90 +13,86 @@ var taskInput = document.getElementById("task");
 
 
 
-function updateToDoCount(size)
-{
-	
-	
-	
-	todoCounts=size;
-	document.getElementById("todoCount").innerHTML=todoCounts;
-	
+function updateToDoCount(size) {
+
+
+
+    todoCounts = size;
+    document.getElementById("todoCount").innerHTML = todoCounts;
+
 }
 
 function addTask() {
-	
-	if(document.getElementById("task").value=="")
-		{
-			alert("Blank Value");
-		}
-	
-	else{
-		
-	
 
-    this.namesList.push(document.getElementById("task").value);
-
-    //document.getElementById("taskDetails").innerHTML= document.getElementById("task").value;
+    if (document.getElementById("task").value == "") {
+        alert("Blank Value");
+    } else {
 
 
 
-    for (var i = 0, n; n = namesList[i]; i++) {
+        this.namesList.push(document.getElementById("task").value);
 
-        var liElement = document.createElement('li');
-
-        var btn = document.createElement('button');
-
-        var checkBox = document.createElement("input"); // checkbox
-
-        var label = document.createElement("label");
-		
-		checkBox.classList.add("col-md-1");
-		
-		liElement.classList.add("li-style", "col-md-12","col-xs-12");
-		
-		liElement.style.cssText='padding-left:0px';
-
-        btn.innerText = "Delete";
-
-        btn.onclick = d;
-		
-		btn.classList.add("btn", "btn-danger" ,"myBtn" , "pull-right","col-md-2");
-
-        checkBox.type = "checkbox";
-
-        checkBox.onchange = c;
-		
-		
+        //document.getElementById("taskDetails").innerHTML= document.getElementById("task").value;
 
 
-        label.value = n;
-		
-		label.innerText = n;
 
-        liElement.id = i;
-		
-		label.classList.add("label-style","col-md-9");
+        for (var i = 0, n; n = namesList[i]; i++) {
 
-        liElement.appendChild(checkBox);
-        liElement.appendChild(label);
-        liElement.appendChild(btn);
+            var liElement = document.createElement('li');
+
+            var btn = document.createElement('button');
+
+            var checkBox = document.createElement("input"); // checkbox
+
+            var label = document.createElement("label");
+
+            checkBox.classList.add("col-md-1");
+
+            liElement.classList.add("li-style", "col-md-12", "col-xs-12");
+
+            liElement.style.cssText = 'padding-left:0px';
+
+            btn.innerText = "Delete";
+
+            btn.onclick = d;
+
+            btn.classList.add("btn", "btn-danger", "myBtn", "pull-right", "col-md-2");
+
+            checkBox.type = "checkbox";
+
+            checkBox.onchange = c;
 
 
+
+
+            label.value = n;
+
+            label.innerText = n;
+
+            liElement.id = i;
+
+            label.classList.add("label-style", "col-md-9");
+
+            liElement.appendChild(checkBox);
+            liElement.appendChild(label);
+            liElement.appendChild(btn);
+
+
+
+        }
+
+        document.getElementById("taskDetails").appendChild(liElement);
+
+        document.getElementById("task").value = "";
+
+
+        document.getElementById("task").focus();
+
+        var size = document.getElementById("taskDetails").childElementCount;
+
+        updateToDoCount(size);
 
     }
-
-    document.getElementById("taskDetails").appendChild(liElement);
-
-    document.getElementById("task").value = "";
-
-
-    document.getElementById("task").focus();
-		
-	var size=document.getElementById("taskDetails").childElementCount;
-		
-	updateToDoCount(size);
-		
-	}
 
 
 
@@ -113,11 +109,11 @@ var c = function completeTask() {
 
     var elm = document.getElementById(this.parentElement.id);
 
-	var size=document.getElementById(elm.id).parentElement.childElementCount;
+    var size = document.getElementById(elm.id).parentElement.childElementCount;
 
     elm.parentNode.removeChild(elm);
-	
-	updateToDoCount(size-1);
+
+    updateToDoCount(size - 1);
 
 
     for (var i = 0, n; n = completedTasks[i]; i++) {
@@ -130,15 +126,21 @@ var c = function completeTask() {
         btn.innerText = "Delete";
 
         btn.onclick = d;
-		
-		btn.classList.add("btn", "btn-danger", "myBtn", "pull-right");
-		
-		liElm.classList.add("li-style");
+
+        btn.classList.add("btn", "btn-danger", "myBtn", "pull-right", "col-md-2");
+
+        liElm.classList.add("li-style");
 
         label.innerText = n;
 		
+		label.classList.add("col-md-10");
 
-        liElm.id = i;
+
+        liElm.id = i+"count";
+		
+		liElm.classList.add("li-style", "col-md-12", "col-xs-12");
+
+            liElm.style.cssText = 'padding-left:0px';
 
         liElm.appendChild(label);
         liElm.appendChild(btn);
@@ -154,14 +156,15 @@ var c = function completeTask() {
 // For deleting tasks
 var d = function deleteTask() {
     alert("Deletion Done");
-
-    console.log("Task Deleted Successfully");
-    console.log(document.getElementById("task"));
-
-    //var lists= document.getElementsByTagName("li");
-    //console.log(lists);
     var elm = document.getElementById(this.parentElement.id);
-	var count=document.getElementById(elm.id).parentElement.childElementCount;
-	elm.parentNode.removeChild(elm);
-	updateToDoCount(count-1);
+    var count = document.getElementById(elm.id).parentElement.childElementCount;
+	var flag= document.getElementById(elm.id).parentElement.id;
+	console.log( document.getElementById(elm.id).parentElement.childElementCount);
+    elm.parentNode.removeChild(elm);
+	//For Updating The Pending Task Counter
+	if(flag=="taskDetails")
+		{
+			 updateToDoCount(count - 1);
+		}
+   
 }
